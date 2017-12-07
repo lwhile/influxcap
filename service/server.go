7bed3d90f0b4ecf73d10fc062ecae1951b68536a
lwh <lwhile521@gmail.com>
2017-12-07 09:32:09 +0800

    add some file

---
+++ service/server.go
@@ -0,0 +1,33 @@
+package service
+
+import "net/http"
+
+// ServerConf contain config of a http server
+type ServerConf struct {
+	Port string
+}
+
+// Server http request
+type Server struct {
+	conf   *ServerConf
+	server http.Server
+}
+
+// NewServer return a Server instance
+func NewServer(conf *ServerConf) *Server {
+	return &Server{
+		conf: conf,
+	}
+}
+
+// Start a http server
+// this method will be blocked
+func (s *Server) Start() error {
+	return s.server.ListenAndServe()
+}
+
+// Stop the http server
+// TODO: implement
+func (s *Server) Stop() error {
+	return nil
+}
